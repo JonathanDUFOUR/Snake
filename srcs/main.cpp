c5c7ae57c6caf9b7df791070aa8539c056f3c349
jodufour <jodufour@student.42.fr>
2021-03-27 01:29:07 +0100

    Thats all for tonight

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jodufour <jodufour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 21:45:02 by jodufour          #+#    #+#             */
-/*   Updated: 2021/03/26 23:53:42 by jodufour         ###   ########.fr       */
+/*   Updated: 2021/03/27 00:59:57 by jodufour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	main(void)
 	snake						player[MAX_SIZE];
 	fruit						fruit;
 	e_directions				dir;
-	std::chrono::milliseconds	timespan(100);
+	std::chrono::milliseconds	timespan(42);
 
 	setup(gameOver, dir, player, fruit, score);
 	while (!gameOver && score < MAX_SIZE)
